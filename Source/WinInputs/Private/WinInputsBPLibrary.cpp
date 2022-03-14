107eb58aa34e0c08b27fd11ffb6fe12106349403
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2022-03-14 03:32:32 +0300

    Initial commit

---
+++ Source/WinInputs/Private/WinInputsBPLibrary.cpp
@@ -0,0 +1,753 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WinInputsBPLibrary.h"
+#include <iostream>
+#include <string>
+#include "Windows/WindowsHWrapper.h"  
+#include "winuser.h"
+#include "WinInputs.h"
+
+UWinInputsBPLibrary::UWinInputsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+bool UWinInputsBPLibrary::PressKeyboardInput(bool ReleaseAfterPress, TEnumAsByte<KeyboardInputs> KeyboardButtons)
+{
+    int Button = 0;
+    
+    switch (KeyboardButtons)
+    {
+    case WinButton:
+        Button = VK_LWIN;
+        break;
+        
+    case LeftAlt:
+        Button = VK_LMENU;
+        break;
+
+    case LeftControl:
+        Button = VK_LCONTROL;
+        break;
+
+    case LeftShift:
+        Button = VK_LSHIFT;
+        break;
+
+    case Tab:
+        Button = VK_TAB;
+        break;
+
+    case Enter:
+        Button = VK_RETURN;
+        break;
+
+    case Space:
+        Button = VK_SPACE;
+        break;
+
+    case ESC:
+        Button = VK_ESCAPE;
+        break;
+    
+    case Delete:
+        Button = VK_DELETE;
+        break;
+
+    case Backspace:
+        Button = VK_BACK;
+        break;
+
+    case CapsLock:
+        Button = VK_CAPITAL;
+        break;
+
+    case Zero_Key:
+        Button = 0x30;
+        break;
+
+    case One_Key:
+        Button = 0x31;
+        break;
+
+    case Two_Key:
+        Button = 0x32;
+        break;
+
+    case Three_Key:
+        Button = 0x33;
+        break;
+
+    case Four_Key:
+        Button = 0x34;
+        break;
+
+    case Five_Key:
+        Button = 0x35;
+        break;
+
+    case Six_Key:
+        Button = 0x36;
+        break;
+
+    case Seven_Key:
+        Button = 0x37;
+        break;
+
+    case Eight_Key:
+        Button = 0x38;
+        break;
+
+    case Nine_Key:
+        Button = 0x39;
+        break;
+
+    case A_Key:
+        Button = 0x41;
+        break;
+
+    case B_Key:
+        Button = 0x42;
+        break;
+
+    case C_Key:
+        Button = 0x43;
+        break;
+
+    case TR_C_Key:
+        Button = 0x220;
+        break;
+
+    case D_Key:
+        Button = 0x44;
+        break;
+
+    case E_Key:
+        Button = 0x45;
+        break;
+
+    case F_Key:
+        Button = 0x46;
+        break;
+
+    case G_Key:
+        Button = 0x47;
+        break;
+
+    case TR_G_Key:
+        Button = 0x219;
+        break;
+
+    case H_Key:
+        Button = 0x48;
+        break;
+
+    case I_Key:
+        Button = 0x49;
+        break;
+
+    case TR_I_Key:
+        Button = 0x222;
+        break;
+
+    case J_Key:
+        Button = 0x4A;
+        break;
+
+    case K_Key:
+        Button = 0x4B;
+        break;
+
+    case L_Key:
+        Button = 0x4C;
+        break;
+
+    case M_Key:
+        Button = 0x4D;
+        break;
+
+    case N_Key:
+        Button = 0x4E;
+        break;
+
+    case O_Key:
+        Button = 0x4F;
+        break;
+
+    case TR_O_Key:
+        Button = 0x191;
+        break;
+
+    case P_Key:
+        Button = 0x50;
+        break;
+
+    case Q_Key:
+        Button = 0x51;
+        break;
+
+    case R_Key:
+        Button = 0x52;
+        break;
+
+    case S_Key:
+        Button = 0x53;
+        break;
+
+    case TR_S_Key:
+        Button = 0x186;
+        break;
+
+    case T_Key:
+        Button = 0x54;
+        break;
+
+    case U_Key:
+        Button = 0x55;
+        break;
+
+    case TR_U_Key:
+        Button = 0x221;
+        break;
+
+    case V_Key:
+        Button = 0x56;
+        break;
+
+    case W_Key:
+        Button = 0x57;
+        break;
+
+    case X_Key:
+        Button = 0x58;
+        break;
+
+    case Y_Key:
+        Button = 0x59;
+        break;
+
+    case Z_Key:
+        Button = 0x5A;
+        break;
+
+    default:
+        Button = 0;
+        break;
+    }
+    
+    if (ReleaseAfterPress == true)
+    {
+        INPUT inputs[2] = {};
+
+        inputs[0].type = INPUT_KEYBOARD;
+        inputs[0].ki.wVk = Button;
+
+        inputs[1].type = INPUT_KEYBOARD;
+        inputs[1].ki.wVk = Button;
+        inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+        UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+        if (SentInput == ARRAYSIZE(inputs))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    else
+    {
+        INPUT inputs[1] = {};
+
+        inputs[0].type = INPUT_KEYBOARD;
+        inputs[0].ki.wVk = Button;
+
+        UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+        if (SentInput == ARRAYSIZE(inputs))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+}
+
+bool UWinInputsBPLibrary::ReleaseKeyboardInput(TEnumAsByte<KeyboardInputs> KeyboardButtons)
+{
+    int Button = 0;
+
+    switch (KeyboardButtons)
+    {
+    case WinButton:
+        Button = VK_LWIN;
+        break;
+
+    case LeftAlt:
+        Button = VK_LMENU;
+        break;
+
+    case LeftControl:
+        Button = VK_LCONTROL;
+        break;
+
+    case LeftShift:
+        Button = VK_LSHIFT;
+        break;
+
+    case Tab:
+        Button = VK_TAB;
+        break;
+
+    case Enter:
+        Button = VK_RETURN;
+        break;
+
+    case Space:
+        Button = VK_SPACE;
+        break;
+
+    case ESC:
+        Button = VK_ESCAPE;
+        break;
+
+    case Delete:
+        Button = VK_DELETE;
+        break;
+
+    case Backspace:
+        Button = VK_BACK;
+        break;
+
+    case CapsLock:
+        Button = VK_CAPITAL;
+        break;
+
+    case Zero_Key:
+        Button = 0x30;
+        break;
+
+    case One_Key:
+        Button = 0x31;
+        break;
+
+    case Two_Key:
+        Button = 0x32;
+        break;
+
+    case Three_Key:
+        Button = 0x33;
+        break;
+
+    case Four_Key:
+        Button = 0x34;
+        break;
+
+    case Five_Key:
+        Button = 0x35;
+        break;
+
+    case Six_Key:
+        Button = 0x36;
+        break;
+
+    case Seven_Key:
+        Button = 0x37;
+        break;
+
+    case Eight_Key:
+        Button = 0x38;
+        break;
+
+    case Nine_Key:
+        Button = 0x39;
+        break;
+
+    case A_Key:
+        Button = 0x41;
+        break;
+
+    case B_Key:
+        Button = 0x42;
+        break;
+
+    case C_Key:
+        Button = 0x43;
+        break;
+
+    case TR_C_Key:
+        Button = 0x220;
+        break;
+
+    case D_Key:
+        Button = 0x44;
+        break;
+
+    case E_Key:
+        Button = 0x45;
+        break;
+
+    case F_Key:
+        Button = 0x46;
+        break;
+
+    case G_Key:
+        Button = 0x47;
+        break;
+
+    case TR_G_Key:
+        Button = 0x219;
+        break;
+
+    case H_Key:
+        Button = 0x48;
+        break;
+
+    case I_Key:
+        Button = 0x49;
+        break;
+
+    case TR_I_Key:
+        Button = 0x222;
+        break;
+
+    case J_Key:
+        Button = 0x4A;
+        break;
+
+    case K_Key:
+        Button = 0x4B;
+        break;
+
+    case L_Key:
+        Button = 0x4C;
+        break;
+
+    case M_Key:
+        Button = 0x4D;
+        break;
+
+    case N_Key:
+        Button = 0x4E;
+        break;
+
+    case O_Key:
+        Button = 0x4F;
+        break;
+
+    case TR_O_Key:
+        Button = 0x191;
+        break;
+
+    case P_Key:
+        Button = 0x50;
+        break;
+
+    case Q_Key:
+        Button = 0x51;
+        break;
+
+    case R_Key:
+        Button = 0x52;
+        break;
+
+    case S_Key:
+        Button = 0x53;
+        break;
+
+    case TR_S_Key:
+        Button = 0x186;
+        break;
+
+    case T_Key:
+        Button = 0x54;
+        break;
+
+    case U_Key:
+        Button = 0x55;
+        break;
+
+    case TR_U_Key:
+        Button = 0x221;
+        break;
+
+    case V_Key:
+        Button = 0x56;
+        break;
+
+    case W_Key:
+        Button = 0x57;
+        break;
+
+    case X_Key:
+        Button = 0x58;
+        break;
+
+    case Y_Key:
+        Button = 0x59;
+        break;
+
+    case Z_Key:
+        Button = 0x5A;
+        break;
+
+    default:
+        Button = 0;
+        break;
+    }
+
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = Button;
+    inputs[0].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::SendKeyboardMacro(TArray<TEnumAsByte<KeyboardInputs>> Array_Buttons)
+{
+    TArray<bool> AnyError;
+    for (int32 ButtonIndex = 0; ButtonIndex < Array_Buttons.Num(); ButtonIndex++)
+    {
+        AnyError.Add(UWinInputsBPLibrary::PressKeyboardInput(false, Array_Buttons[ButtonIndex]));
+    }
+
+    for (int32 ButtonIndex = 0; ButtonIndex < Array_Buttons.Num(); ButtonIndex++)
+    {
+        AnyError.Add(UWinInputsBPLibrary::ReleaseKeyboardInput(Array_Buttons[ButtonIndex]));
+    }
+
+    if (AnyError.Contains(false) == true)
+    {
+        return false;
+    }
+
+    else
+    {
+        return true;
+    }
+}
+
+void UWinInputsBPLibrary::GetActiveWindows(TArray<FString>& WindowsNames)
+{
+    for (HWND WindowHandle = GetTopWindow(NULL); WindowHandle != NULL; WindowHandle = GetNextWindow(WindowHandle, GW_HWNDNEXT))
+    {
+        int WindowTitleLenght = GetWindowTextLength(WindowHandle);
+        if (WindowTitleLenght == 0)
+            continue;
+
+        if (!IsWindowVisible(WindowHandle))
+            continue;
+
+        char* EachWindowTitle = new char[WindowTitleLenght + 1];
+        GetWindowTextA(WindowHandle, EachWindowTitle, WindowTitleLenght + 1);
+
+        WindowsNames.Add(EachWindowTitle);
+    }
+}
+
+bool UWinInputsBPLibrary::BringWindowFront(const FString WindowString)
+{
+    const CHAR* WindowChar = TCHAR_TO_ANSI(*WindowString);
+    HWND WindowHandle = FindWindowA(NULL, WindowChar);
+    return BringWindowToTop(WindowHandle);
+}
+
+void UWinInputsBPLibrary::SetMousePosition(int32 In_Pos_X, int32 In_Pos_Y, int32 Widget_Size_X, int32 Widget_Size_Y, int32& Out_Pos_X, int32& Out_Pos_Y)
+{
+    int32 Desktop_Resolution_X = GetSystemMetrics(SM_CXSCREEN);
+    int32 Desktop_Resolution_Y = GetSystemMetrics(SM_CYSCREEN);
+
+    float DPI_X = Desktop_Resolution_X / Widget_Size_X;
+    float DPI_Y = Desktop_Resolution_Y / Widget_Size_Y;
+
+    Out_Pos_X = In_Pos_X * DPI_X;
+    Out_Pos_Y = In_Pos_Y * DPI_Y;
+
+    POINT MousePosition;
+    MousePosition.x = Out_Pos_X;
+    MousePosition.y = Out_Pos_Y;
+
+    SetCursorPos(Out_Pos_X, Out_Pos_Y);
+}
+
+bool UWinInputsBPLibrary::MousePressLeft()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MouseReleaseLeft()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MousePressRight()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MouseReleaseRight()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MousePressMiddle()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MouseReleaseMiddle()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MouseWheelUp()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
+    inputs[0].mi.mouseData = 120;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UWinInputsBPLibrary::MouseWheelDown()
+{
+    INPUT inputs[1] = {};
+
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
+    inputs[0].mi.mouseData = -120;
+
+    UINT SentInput = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    if (SentInput == ARRAYSIZE(inputs))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
---
+++ Source/WinInputs/Public/WinInputsBPLibrary.h
@@ -0,0 +1,131 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WinInputsBPLibrary.generated.h"
+
+/* 
+*	Function library class.
+*	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
+*
+*	When declaring function you can define metadata for the node. Key function specifiers will be BlueprintPure and BlueprintCallable.
+*	BlueprintPure - means the function does not affect the owning object in any way and thus creates a node without Exec pins.
+*	BlueprintCallable - makes a function which can be executed in Blueprints - Thus it has Exec pins.
+*	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
+*				Its lets you name the node using characters not allowed in C++ function names.
+*	CompactNodeTitle - the word(s) that appear on the node.
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*				Good example is "Print String" node which you can find also by using keyword "log".
+*	Category -	the category your node will be under in the Blueprint drop-down menu.
+*
+*	For more info on custom blueprint nodes visit documentation:
+*	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
+*/
+
+UENUM()
+enum KeyboardInputs
+{
+	WinButton		UMETA(DisplayName = "Windows"),
+	LeftAlt			UMETA(DisplayName = "Left Alt"),
+	LeftControl		UMETA(DisplayName = "Left Control"),
+	LeftShift		UMETA(DisplayName = "Left Shift"),
+	Tab				UMETA(DisplayName = "Tab"),
+	Enter			UMETA(DisplayName = "Enter"),
+	Space			UMETA(DisplayName = "Space"),
+	ESC				UMETA(DisplayName = "Escape"),
+	Delete			UMETA(DisplayName = "Delete"),
+	Backspace		UMETA(DisplayName = "Backspace"),
+	CapsLock		UMETA(DisplayName = "Caps Lock"),
+	Zero_Key		UMETA(DisplayName = "0"),
+	One_Key			UMETA(DisplayName = "1"),
+	Two_Key			UMETA(DisplayName = "2"),
+	Three_Key		UMETA(DisplayName = "3"),
+	Four_Key		UMETA(DisplayName = "4"),
+	Five_Key		UMETA(DisplayName = "5"),
+	Six_Key			UMETA(DisplayName = "6"),
+	Seven_Key		UMETA(DisplayName = "7"),
+	Eight_Key		UMETA(DisplayName = "8"),
+	Nine_Key		UMETA(DisplayName = "9"),
+	A_Key			UMETA(DisplayName = "A"),
+	B_Key			UMETA(DisplayName = "B"),
+	C_Key			UMETA(DisplayName = "C"),
+	TR_C_Key		UMETA(DisplayName = "Ç"),
+	D_Key			UMETA(DisplayName = "D"),
+	E_Key			UMETA(DisplayName = "E"),
+	F_Key			UMETA(DisplayName = "F"),
+	G_Key			UMETA(DisplayName = "G"),
+	TR_G_Key		UMETA(DisplayName = "Ğ"),
+	H_Key			UMETA(DisplayName = "H"),
+	I_Key			UMETA(DisplayName = "I"),
+	TR_I_Key		UMETA(DisplayName = "İ"),
+	J_Key			UMETA(DisplayName = "J"),
+	K_Key			UMETA(DisplayName = "K"),
+	L_Key			UMETA(DisplayName = "L"),
+	M_Key			UMETA(DisplayName = "M"),
+	N_Key			UMETA(DisplayName = "N"),
+	O_Key			UMETA(DisplayName = "O"),
+	TR_O_Key		UMETA(DisplayName = "Ö"),
+	P_Key			UMETA(DisplayName = "P"),
+	Q_Key			UMETA(DisplayName = "Q"),
+	R_Key			UMETA(DisplayName = "R"),
+	S_Key			UMETA(DisplayName = "S"),
+	TR_S_Key		UMETA(DisplayName = "Ş"),
+	T_Key			UMETA(DisplayName = "T"),
+	U_Key			UMETA(DisplayName = "U"),
+	TR_U_Key		UMETA(DisplayName = "Ü"),
+	V_Key			UMETA(DisplayName = "V"),
+	W_Key			UMETA(DisplayName = "W"),
+	X_Key			UMETA(DisplayName = "X"),
+	Y_Key			UMETA(DisplayName = "Y"),
+	Z_Key			UMETA(DisplayName = "Z"),
+};
+
+UCLASS()
+class UWinInputsBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Press Keyboard Input", ToolTip = "Description.", Keywords = "press, keyboard, input"), Category = "Win Inputs")
+	static bool PressKeyboardInput(bool ReleaseAfterPress, TEnumAsByte<KeyboardInputs> KeyboardButtons);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Release Keyboard Input", ToolTip = "Description.", Keywords = "release, keyboard, input"), Category = "Win Inputs")
+	static bool ReleaseKeyboardInput(TEnumAsByte<KeyboardInputs> KeyboardButtons);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Send Keyboard Macro", ToolTip = "Description.", Keywords = "send, keyboard, input, macro"), Category = "Win Inputs")
+	static bool SendKeyboardMacro(TArray<TEnumAsByte<KeyboardInputs>> Array_Buttons);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Active Windows", ToolTip = "Description.", Keywords = "get, active, windows"), Category = "Win Inputs")
+	static void GetActiveWindows(TArray<FString>& WindowsNames);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Bring Window Front", ToolTip = "Description.", Keywords = "bring, window, front"), Category = "Win Inputs")
+	static bool BringWindowFront(const FString WindowString);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set Mouse Position", ToolTip = "Description.", Keywords = "set, mouse, position, location"), Category = "Win Inputs")
+	static void SetMousePosition(int32 In_Pos_X, int32 In_Pos_Y, int32 Widget_Size_X, int32 Widget_Size_Y, int32& Out_Pos_X, int32& Out_Pos_Y);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Press Left", ToolTip = "Description.", Keywords = "press, mouse, left"), Category = "Win Inputs")
+	static bool MousePressLeft();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Release Left", ToolTip = "Description.", Keywords = "release, mouse, left"), Category = "Win Inputs")
+	static bool MouseReleaseLeft();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Press Right", ToolTip = "Description.", Keywords = "press, mouse, right"), Category = "Win Inputs")
+	static bool MousePressRight();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Release Right", ToolTip = "Description.", Keywords = "release, mouse, right"), Category = "Win Inputs")
+	static bool MouseReleaseRight();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Press Middle", ToolTip = "Description.", Keywords = "press, mouse, middle"), Category = "Win Inputs")
+	static bool MousePressMiddle();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Release Middle", ToolTip = "Description.", Keywords = "release, mouse, middle"), Category = "Win Inputs")
+	static bool MouseReleaseMiddle();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Wheel Up", ToolTip = "Description.", Keywords = "wheel, mouse, up"), Category = "Win Inputs")
+	static bool MouseWheelUp();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Mouse Wheel Down", ToolTip = "Description.", Keywords = "wheel, mouse, down"), Category = "Win Inputs")
+	static bool MouseWheelDown();
+
+};
